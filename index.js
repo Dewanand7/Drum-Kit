880b290e241860f49f1cd7efcc8904e291856810
Dewanand khilar <dewanandkhilar@gmail.com>
2021-11-24 12:28:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+for  (var i = 0; i<numberOfDrumButton; i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+alert("Yes i am clicked!");
+});
+}
